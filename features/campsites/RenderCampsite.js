5eb180e07d4400a44cba1451d6df2082356f5b2b
Sean Bradley <sb_210@hotmail.com>
2023-09-19 18:04:23 -0400

    Icon, Favorites, and Comments

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,14 +1,20 @@
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 
-// campsite prop being passed in comes from MainComponent.js where campsite is the variable declared by useState with the initial value of the CAMPSITES array.
+
 // Card - returns a card component
 // Double Curly Braces for style=margin:20 mean; Outer cruly braces denote javascript being used in JSX, inner curly braces deonte a JS object.
+// Icon with icon sets (name, type, etc) raised prop = subtle shadow effect, reverse prop = reverse the color scheme. See Icon documentation for a list of sets and how to use. 
+// Icon name= ternary operator checking props.isFavorite, returning string 'heart' if true, 'heart-o' if false.
+// (props) - passing in the entire props object in as the parameter so we can have access to all the props we pass in, not just the campsite prop like before.
+// campsite prop under RenderCampsite is destructuring the campsite object and comes from MainComponent.js where campsite is the variable declared by useState with the initial value of the CAMPSITES array.
+// onPress= - calling the markFavoriteEventHandler we're receiving through props to tell the campsiteinfo screen, a heart icon has been clicked on, which will call the function we set = to the markFavorite prop when we passed it in from the campsiteinfo screen.
 
-const RenderCampsite = ({ campsite }) => {
+const RenderCampsite = (props) => {
+    const { campsite } = props;
     if (campsite) {
         return (
-            <Card containerStyle={{ padding: 0 }}>
+            <Card containerStyle={ styles.cardContainer }>
                 <Card.Image source={campsite.image}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
@@ -23,10 +29,32 @@ const RenderCampsite = ({ campsite }) => {
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
+                <Icon
+                    name={props.isFavorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() =>
+                        props.isFavorite
+                            ? console.log('Already set as a favorite')
+                            : props.markFavorite()
+                    }
+                />
             </Card>
         );
     }
     return <View />;
 };
+
+// StyleSheet: creating a stylesheet using StyleSheet.create call and passing in an object with the cardContainer style.
+
+const styles = StyleSheet.create({
+    cardContainer: {
+    padding: 0,
+    margin: 0,
+    marginBottom: 20
+    }
+    });
 
 export default RenderCampsite;
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { COMMENTS } from '../shared/comments';
 
-
 // Navigation and route props are available via react.  route will be used to ge the navigation params passed to the navigation function back in the directory screen.
 // route: destructuring the route prop in the parameter list of the CampsiteInfoScreen function.
 // const campsite: destructuring to get the campsite param from the route.params object, which is where all the params for this route are located.
@@ -13,18 +12,26 @@ import { COMMENTS } from '../shared/comments';
 // data=comments.filter: filtering all the comments from the comments array that have an id that matches the id from the campsite we're currently rendering. (campsite.id coming from route prop.) This will result in array of comments that match this particular comapsites ID.
 // style=styles.commentItem: custom style in the stylesheet, it's a javascript object.
 // fontSize: 14 - When you use double curly braces, the first one is telling that you are going to write JavaScript, and the second one is creating a JavaScript object. This is because the style attribute expects a JavaScript object as its value.
-// RenderCampsite campsite=item: inside the campsite prop of the RenderCampsite, setting the campsite prop = to the campsite data received through route.params. Curly braces around item indicate we're destructuring the item property from the object passed in an an argument to the function..
 // item.text: only 1 set of curly braces because item.text isn't an object but rather, it's a reference to the `text` property of the `item` object.
+// item.author, item.data: template literal syntax, add dashes and a comma to results.
+// keyExtractor prop - set the comments to use their unique Id they have, for the key.
+// contentContainerStyle - since flatList is rendering the entire content, use special prop of contentContainerStyle to add styles.
+// marginHorizontal: add space to sided, padding Vertical: space on top & bottom.
+// ListHeaderComponent: shows content above the flatlist, can also use the other built in component, ListFooterComponent. ListHeader receives a single argument (campsite).
+// RenderCampsite: displays campsite prop in a title above, both the title and text are wrapped in a fragment so we're only passing in a single parent component. 
+// Temporary - useState(false) - state variable used to track if the campsite is marked as a favorite.
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const [comments, setComments] = useState(COMMENTS);
+    const [favorite, setFavorite] = useState(false);
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14}}>{item.text}</Text>
                 <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
             </View>
         )
     }
@@ -39,12 +46,33 @@ const CampsiteInfoScreen = ({ route }) => {
             contentContainerStyle={{ marginHorizontal: 20, paddingVertical:20 }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite campsite={campsite} />
-                    <Text style={StyleSheet.commentsTitle}>Comments</Text>
+                    <RenderCampsite 
+                        campsite={campsite} 
+                        isFavorite={favorite}
+                        markFavorite={() => setFavorite(true)}
+                    />
+                    <Text style={styles.commentsTitle}>Comments</Text>
                 </>
             }
         />
     );
 };
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#434840',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    }
+}); 
 
 export default CampsiteInfoScreen;

4c1a5333bc5e6b8883d65e957bdc20a98a6f779c
Sean Bradley <sb_210@hotmail.com>
2023-09-28 13:36:51 -0400

    swipe and delete favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,8 +1,10 @@
-import { useSelector } from 'react-redux';
-import { View, FlatList, Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { View, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import Loading from '../components/LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { SwipeRow } from 'react-native-swipe-list-view';
+import { toggleFavorite } from '../features/favorites/favoritesSlice'
 
 // navigation - destructuring the navigation prop. This navigation prop is given to this component by react navigation when we add it as a screen in the stack navigator.
 // useSelector hook - getting state data from redux store. deconstruct those 3 from the return value of useSelector, returning the state.campsites portion of our state.
@@ -21,31 +23,54 @@ import { baseUrl } from '../shared/baseUrl';
 // navigate - Need to tell the navigate method, what navigator to use (Directory) and what screen to show within that navigator (CampsiteInfo), don't have to do this if navigating in the same navigator like from directory screen to campsiteInfo screen.
 // Directory (first argument) - name of top level navigator we want to navigate to. (this is the name prop we set in drawer navigators drawer.screen tag for the directory navigator).
 // second argument - configuration object, params - set to an object with a key of campsites = our campsite object, object property shortand.
+// dispatch - for updating the redux store
+// SwipeRow - let us make the ListItem component swipeable. It expects 2 view components. 1st view - hidden view (after you swipe), 2nd view - default view before you swipe.
+// rightOpenValue - SwipeRow will open on the right side.  Requires a negative integer who value represents the amount of pixels the user will have to swipe for the row to open (from right to left). so the ListItem goes into the second view.
+// TouchableOpacity - our hidden delete options
+// onPress - when the onPress eventListener prop is pressed, then the dispatch function passes a call to toggleFavorite action creator, passing in campsite.id as the payload.
 
 const FavoritesScreen = ({ navigation }) => {
     const { campsitesArray, isLoading, errMess } = useSelector(
         (state) => state.campsites
     );
     const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
 
     const renderFavoriteItem = ({ item: campsite }) => {
         return (
-            <ListItem
-                onPress={() => navigation.navigate('Directory', {
-                    screen: 'CampsiteInfo',
-                    params: { campsite }
-                })}
-            >
-                <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>
-                        {campsite.description}
-                    </ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
-        )
-    }
+            <SwipeRow rightOpenValue={-100}>
+                <View style={styles.deleteView}>
+                    <TouchableOpacity
+                        style={styles.deleteTouchable}
+                        onPress={() => dispatch(toggleFavorite(campsite.id))}
+                    >
+                        <Text style={styles.deleteText}>Delete</Text>
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <ListItem
+                        onPress={() =>
+                            navigation.navigate('Directory', {
+                                screen: 'CampsiteInfo',
+                                params: { campsite }
+                            })
+                        }
+                    >
+                        <Avatar
+                            rounded
+                            source={{ uri: baseUrl + campsite.image }}
+                        />
+                        <ListItem.Content>
+                            <ListItem.Title>{campsite.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {campsite.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                </View>
+            </SwipeRow>
+        );
+    };
 
     if (isLoading) {
         return <Loading />;
@@ -65,6 +90,28 @@ const FavoritesScreen = ({ navigation }) => {
             renderItem={renderFavoriteItem}
             keyExtractor={(item) => item.id.toString()}
         />
-    )
+    );
 };
+
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+});
+
 export default FavoritesScreen;

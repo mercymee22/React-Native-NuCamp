cb3d8faf6dea60d1fcd0327e5dfed259a03821ab
Sean Bradley <sb_210@hotmail.com>
2023-09-29 15:49:53 -0400

    troubleshoot 4

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -14,44 +14,40 @@ const RenderCampsite = (props) => {
     const { campsite } = props;
     if (campsite) {
         return (
-            <Card containerStyle={styles.cardContainer}>
-                <Card.Image source={{ uri: baseUrl + campsite.image }}>
-                    <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text style={styles.cardText}>
-                            {campsite.name}
-                        </Text>
+                <Card containerStyle={styles.cardContainer}>
+                    <Card.Image source={{ uri: baseUrl + campsite.image }}>
+                        <View style={{ justifyContent: 'center', flex: 1 }}>
+                            <Text style={styles.cardText}>{campsite.name}</Text>
+                        </View>
+                    </Card.Image>
+                    <Text style={{ margin: 20 }}>{campsite.description}</Text>
+                    <View style={styles.cardRow}>
+                        <Icon
+                            name={props.isFavorite ? 'heart' : 'heart-o'}
+                            type='font-awesome'
+                            color='#f50'
+                            raised
+                            reverse
+                            onPress={() =>
+                                props.isFavorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                            }
+                        />
+                        <Icon
+                            name='pencil'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={props.onShowModal}
+                        />
                     </View>
-                </Card.Image>
-                <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <View style={styles.cardRow}>
-                    <Icon
-                        name={props.isFavorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        raised
-                        reverse
-                        onPress={() =>
-                            props.isFavorite
-                                ? console.log('Already set as a favorite')
-                                : props.markFavorite()
-                        }
-                    />
-                    <Icon
-                        name='pencil'
-                        color='#5637DD'
-                        raised
-                        reverse
-                        onPress={() => props.onShowModal()}
-                    >
-                    </Icon>
-                </View>
-            </Card>
+                </Card>
         );
     }
     return <View />;
 };
-
-// StyleSheet: creating a stylesheet using StyleSheet.create call and passing in an object with the cardContainer style.
 
 const styles = StyleSheet.create({
     cardContainer: {
@@ -67,8 +63,8 @@ const styles = StyleSheet.create({
         margin: 20
     },
     cardText: {
-        textShadowColor: 'rgba(0,0,0,1)',
-        textShadowOffset: { width: -1, height: 1},
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowOffset: { width: -1, height: 1 },
         textShadowRadius: 20,
         textAlign: 'center',
         color: 'white',
